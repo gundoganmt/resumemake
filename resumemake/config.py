6988879244c8e06ca13f41cce5e322d9af88cadf
gundoganm <31245314+gundoganmt@users.noreply.github.com>
2022-03-31 10:38:31 +0300

    Update config.py

--- resumemake/config.py
+++ resumemake/config.py
@@ -1,13 +1,10 @@
 import psycopg2, os
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 class Config:
-    SECRET_KEY = '7c6b7967-dcba-4796-a261-f36b028144e3'
-    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:WulIgtM5zk@localhost/resumemake"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = "smtp.zoho.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = "admin@webpaget.com"
-    MAIL_PASSWORD = "UpG6v6Z1Cj23"
+    SECRET_KEY = '7c6b7967-dcba-4796-a261-f36b028144e3' # random secret key
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024
+    SQLALCHEMY_DATABASE_URI = uri
